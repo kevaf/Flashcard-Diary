aeca629b52586b85120c7469ea34679e4d0f90c8
kevaf <kevinkipyegon20@gmail.com>
2021-10-28 15:02:47 +0300

    made changes to urls

--- cards/urls.py
+++ cards/urls.py
@@ -6,7 +6,6 @@ from knox import views as knox_views
 
 
 urlpatterns=[
-     url(r'^$',LoginAPI.as_view(), name='login'),
      url(r'^api/cards/$', views.CardList.as_view()),
      url(r'api/cards/card-id/(?P<pk>[0-9]+)/$', views.CardDets.as_view()),
      path('api/register/', RegisterAPI.as_view(), name='register'),
